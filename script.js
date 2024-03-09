98831c3fd21b91f623005fe6f40114448938eeb1
js1 <alikulovbahadir06@gmail.com>
2024-03-09 23:35:33 +0500

    firstJS

---
+++ script.js
@@ -0,0 +1,29 @@
+var str = prompt('Введите свое имя') 
+var str2 = prompt('Введите свой возраст') 
+var str3 = +prompt('10 + 5') 
+var str4 = prompt('8 - 5')
+var str5 = prompt('9 * 3')
+var str6 = prompt('72 / 9')
+var str7 = prompt('16 % 5')
+var str8 = alert('Откройте консоль')
+var X = +prompt('Введите первое число')
+var Y= +prompt('Введите второе число')
+var Z = +prompt('Введите третье число')
+var res = ((X + Y + Z) / 3)
+var str9 = alert('Откройте консоль')
+
+
+ 
+
+console.log('Меня зовут ' + str);
+console.log('Мне ' + str2 + ' лет');
+console.log('Пример №1 : ' + '10 + 5 ' + '= ' + (10+5) + ' Ваш ответ ' + str3 );
+console.log('Пример №2 : ' + '8 - 5 ' + '= ' + (8-5) + ' Ваш ответ ' + str4);
+console.log('Пример №3 : ' + '9 * 3 ' + '= ' + (9*3) + ' Ваш ответ ' + str5);
+console.log('Пример №4 : ' + '72 / 9 ' + '= ' + (72/9) + ' Ваш ответ ' + str6);
+console.log('Пример №5 : ' + '16 % 5 ' + '= ' + (16%5) + ' Ваш ответ ' + str7);
+console.log('Ваше первое число ' + X );
+console.log('Ваше второе число ' + Y);
+console.log('Ваше третье число ' + Z);
+
+console.log('Среднее арифметическое ваших чисел ' + '= ' + res);
